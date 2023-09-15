2353fdd064aba84ca2cdfb43446f1bb593b51758
farhanAfrad <farhanafrad796@gmail.com>
2023-09-15 17:20:14 +0600

    setup for assignment with react template including tailwind css framework is completed. to check whether everything is working fine header section is also included with inter google font

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div className="pt-12 pb-8 w-11/12 mx-auto text-center">
+            <h1 className="text-3xl font-bold">Course Registration</h1>
+        </div>
+    );
+};
+
+export default Header;
