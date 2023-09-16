0fcf452bff18a21d6662dece55cd439e6b1feab9
farhanAfrad <farhanafrad796@gmail.com>
2023-09-16 14:19:37 +0600

    toast added based on some condition where users can not exceed 20 credit hour and same course can not be selected multiple time

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,42 @@ import Header from './components/Header/Header'
 import Courses from './components/Courses/Courses'
 import Cart from './components/Cart/Cart'
 import { useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [courseIncart,setCourseCart] = useState([]);
   const [totalCredit, setTotlaCredit] = useState(0);
+  const [remainingCredit,setRemainingCredit] = useState(20);
   let creditHour = totalCredit;
+  let remainingCreditHour = remainingCredit;
   const handleSelectToCart = (course) =>{
    const alreadyExist = courseIncart.find(crs => crs.id === course.id);
    console.log(alreadyExist);
    if(alreadyExist){
-    return alert('you already added this course')
+    toast('you already added this course')
    }
    else{
       creditHour += course.credit_hour;
+      remainingCreditHour -= course.credit_hour;
       if(creditHour <= 20){
         const newCart = [...courseIncart,course];
         setCourseCart(newCart);
         setTotlaCredit(creditHour);
+        setRemainingCredit(remainingCreditHour);
       }
       else{
-        return alert('you can not exceed 20 credit hour');
-      }
+        toast.error('you can not exceed 20 credit hour!', {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          })
+        }
    }   
   }
    console.log(totalCredit);
@@ -34,7 +49,18 @@ function App() {
       <Header></Header>
       <main className='w-11/12 mx-auto md:flex gap-6'>
         <Courses handleSelectToCart={handleSelectToCart}></Courses>
-        <Cart courseIncart={courseIncart} totalCredit={totalCredit}></Cart>
+        <Cart courseIncart={courseIncart} totalCredit={totalCredit} remainingCredit={remainingCredit}></Cart>
+        <ToastContainer 
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"/>
       </main>
     </>
   )
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,12 +2,12 @@
 import PropTypes from 'prop-types'
 // import List from '../List/List';
 
-const Cart = ({courseIncart,totalCredit}) => {
+const Cart = ({courseIncart,totalCredit,remainingCredit}) => {
     // const {credit_hour} = courseIncart;
     // courseIncart.forEach(course => totalCredit += course.credit_hour);
     return (
         <div className="w-1/4 bg-white p-6 rounded-xl h-min">
-            <h3 className="mb-4 text-lg font-bold text-blue-500">Credit Hour Remaining 7 hr</h3>
+            <h3 className="mb-4 text-lg font-bold text-blue-500">Credit Hour Remaining {remainingCredit} hr</h3>
             <div className="pt-4 pb-6 border-t-2 border-b-2">
                 <h2 className="text-xl font-bold">Course Name</h2>
                 <ol className="list-decimal ml-5 mt-5 text-gray-700">
@@ -19,13 +19,14 @@ const Cart = ({courseIncart,totalCredit}) => {
             </div>
             
             <p className="py-4 border-b-2 text-gray-600 font-semibold">Total Credit Hour : {totalCredit}</p>
-            <p className="py-4 text-gray-600 font-semibold">Total Price : 48000 USD</p>
+           
         </div>
     );
 };
 
 Cart.propTypes = {
     courseIncart: PropTypes.array,
-    totalCredit: PropTypes.number
+    totalCredit: PropTypes.number,
+    remainingCredit: PropTypes.number
 }
 export default Cart;
